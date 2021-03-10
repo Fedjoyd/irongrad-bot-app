f2dd1423796a285974d6a6adb637ce29ac614404
Gabriel FREMONT <g.fremont@student.isartdigital.com>
2021-03-10 23:50:59 +0100

    add twitch chat bot

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
+// ------ discord ------
+//
 const Discord = require("discord.js");
 const client = new Discord.Client();
-require("dotenv").config();
 
 client.on("ready", () => {
     console.log("I'm ready !");
@@ -12,5 +15,30 @@ client.on("message", msg => {
     }
 });
 
-console.log("bot token : " + process.env.BOT_TOKEN);
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+// ------ twitch ------
+//
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: 'IrongradBot',
+		password: ('oauth:' + process.env.TWITCH_OAUTH_TOKEN)
+	},
+	channels: [ 'irongradtv' ]
+});
+
+client.connect().catch(console.error);
+
+client.on('message', (channel, tags, message, self) => {
+	if(self) return;
+	if(message.toLowerCase() === '!hello') {
+		client.say(channel, `@${tags.username}, heya!`);
+	}
+});
